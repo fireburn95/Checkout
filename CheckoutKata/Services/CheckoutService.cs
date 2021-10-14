44bf3c7863ec7b4d93095ec28a21ac2fd1175517
Abbas <p13223165@my365.dmu.ac.uk>
2021-10-14 22:30:48 +0100

    Added methods for checkout service

--- CheckoutKata/Services/CheckoutService.cs
+++ CheckoutKata/Services/CheckoutService.cs
@@ -2,6 +2,29 @@ namespace CheckoutKata.Models
 {
     public class CheckoutService : ICheckoutService
     {
-        
+        public void AddProduct(Product product)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveProduct(Product product)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ClearProducts(Product product)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ClearBasket()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int CompleteOrder()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- CheckoutKata/Services/ICheckoutService.cs
+++ CheckoutKata/Services/ICheckoutService.cs
@@ -2,6 +2,14 @@ namespace CheckoutKata.Models
 {
     public interface ICheckoutService
     {
-        
+        public void AddProduct(Product product);
+
+        public void RemoveProduct(Product product);
+
+        public void ClearProducts(Product product);
+
+        public void ClearBasket();
+
+        public int CompleteOrder();
     }
 }
